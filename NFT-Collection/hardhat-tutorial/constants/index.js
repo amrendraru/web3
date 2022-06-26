74a06b85330f9912e1c388ad137bcc795bbc7496
amrendraru <amrendrasingh@live.com>
2022-06-26 11:35:45 +0530

    NFT Collection

---
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+
+const WHITELIST_CONTRACT_ADDRESS = "0xc20E365b971df029Ae5CD297B71db9B016cd508D";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
